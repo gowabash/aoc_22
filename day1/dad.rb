167320c0917fdc6b34a41a029e1c1b8b4a8c6612
Matt Brown <matt.brown@seismic.com>
2022-12-01 18:15:26 -0500

    solved star 1

---
+++ day1/dad.rb
@@ -0,0 +1,36 @@
+#! /usr/bin/env ruby
+
+class Elf
+  def initialize
+    @carry = []
+  end
+
+  def add(item)
+    @carry << item
+  end
+
+  def sum
+    @carry.sum
+  end
+end
+
+
+
+lines = File.readlines('input.dad')
+
+elves = []
+elf = Elf.new
+
+lines.each do |line|
+  line.chomp!
+  if line == ""
+    elves << elf.sum
+    elf = Elf.new
+    next
+  else
+    elf.add line.to_i
+  end
+end
+elves << elf.sum
+
+puts elves.sort.last
