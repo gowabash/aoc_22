fdd2decef731bd577f924b7ab2a6a37f5a46ab95
Matt Brown <matt.brown@seismic.com>
2022-12-03 11:57:21 -0500

    2nd star

--- day2/dad.rb
+++ day2/dad.rb
@@ -1,14 +1,6 @@
 #! /usr/bin/env ruby
 
-# A,X - rock
-# B,Y - paper
-# C,Z - scissors
-# win - 6
-# draw - 3
-# loss - 0
 
-
-lines = File.readlines('input.dad')
 def shift_me(choice)
   choice.ord - 87
 end
@@ -24,16 +16,58 @@ def winner(b, a)
   return 0
 end
 
-def calc(line)
+def calc1(line)
   you, me = line.split
   sum = 0
   sum += shift_me(me)
   sum += winner(shift_you(you), shift_me(me))
 end
 
-sum = 0
-lines.each do |line|
-  sum += calc(line)
+# 1 is loose
+# 2 draw
+# 3 win
+def pick(you, result)
+  return you if result == 2
+  return (you%3) + 1 if result == 3
+  return (((you - 1)+2) % 3) + 1 if result == 1
 end
 
-puts sum
+def calc2(line)
+  you, me = line.split
+  sum = 0
+  sum += (shift_me(me) - 1) * 3
+  sum += pick(shift_you(you), shift_me(me))
+end
+
+
+def main
+  sum = 0
+  lines = File.readlines('input.dad')
+  lines.each do |line|
+    sum += calc2(line)
+  end
+
+  puts sum
+end
+
+# A,X - rock
+# B,Y - paper
+# C,Z - scissors
+# win - 6
+# draw - 3
+# loss - 0
+
+def test
+  line = "A Y"
+  you, me = line.split
+  puts "This should be 4 -> " + calc2("A Y").to_s
+  puts "This should be 3 -> " + calc2("A X").to_s
+  puts "This should be 1 -> " + calc2("B X").to_s
+  puts "This should be 2 -> " + calc2("C X").to_s
+  puts "This should be 8 -> " + calc2("A Z").to_s
+  puts "This should be 9 -> " + calc2("B Z").to_s
+  puts "This should be 7 -> " + calc2("C Z").to_s
+end
+
+main
+#test
