213546002adfc46f3dc24e028dcbb97ecbb0c4b4
Matt Brown <matt.brown@seismic.com>
2022-12-05 16:11:17 -0500

    2nd star

--- day4/dad.rb
+++ day4/dad.rb
@@ -1,19 +1,31 @@
 #! /usr/bin/env ruby
 
 def calc1(line)
-  puts line
   a,b = line.split(',')
   mina, maxa = a.split('-')
   minb, maxb = b.split('-')
 
   if (mina.to_i <= minb.to_i) && (maxa.to_i >= maxb.to_i)
-    puts "a"
     return 1 
   elsif mina.to_i >= minb.to_i && maxa.to_i <= maxb.to_i
-    puts "b"
     return 1 
   else
-    puts "c"
+    return 0
+  end
+end
+
+def calc2(line)
+  a,b = line.split(',')
+  mina, maxa = a.split('-')
+  minb, maxb = b.split('-')
+
+  if (mina.to_i >= minb.to_i) && (mina.to_i <= maxb.to_i)
+    return 1 
+  elsif (maxa.to_i >= minb.to_i) && (maxa.to_i <= maxb.to_i)
+    return 1 
+  elsif (mina.to_i < minb.to_i) && (maxb.to_i <= maxa.to_i)
+    return 1 
+  else
     return 0
   end
 end
@@ -28,4 +40,14 @@ def main1
   puts sum
 end
 
-main1
+def main2
+  sum = 0
+  lines = File.readlines('input.dad')
+  lines.each do |line|
+    sum += calc2(line)
+  end
+
+  puts sum
+end
+
+main2
